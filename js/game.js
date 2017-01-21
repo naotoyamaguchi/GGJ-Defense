0ce24bd7ada27f15cfc6a6346b9b416ff1769b3a
Naoto Yamaguchi <naotoy@hawaii.edu>
2017-01-21 09:55:30 -1000

    initial commit

---
+++ js/game.js
@@ -0,0 +1,21 @@
+(Phaser => {
+  const GAME_WIDTH = 460;
+  const GAME_HEIGHT = 600;
+  const GAME_CONTAINER_ID = 'game';
+
+  const preload = _ => {
+
+  };
+
+  const create = _ => {
+
+  };
+
+  const update = _ => {
+
+  };
+
+  const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
+
+
+})(window.Phaser);
